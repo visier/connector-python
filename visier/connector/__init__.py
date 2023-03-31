32b15271688d570fb7ed399f8ca7b719a479ae0c
Henrik Areskoug <henrik.areskoug@visier.com>
2023-03-31 13:36:54 -0700

    additional exports

--- visier/connector/__init__.py
+++ visier/connector/__init__.py
@@ -3,10 +3,8 @@
 # flake8: noqa
 
 """
-    Visier Public Python Connection
+    Visier Public Python Connector
 """
 
-__version__ = "0.0.1"
-
 from .authentication import Authentication
-from .sessions import VisierSession
+from .sessions import VisierSession, ResultTable, QueryExecutionError
