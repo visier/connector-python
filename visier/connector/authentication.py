67b7bcef833c6b2222aac7b63318181162b65158
Henrik Areskoug <henrik.areskoug@visier.com>
2023-08-31 17:40:37 -0700

    Align file license format

--- visier/connector/authentication.py
+++ visier/connector/authentication.py
@@ -1,15 +1,16 @@
-# This file is part of visier-connector-python.
-#
-# visier-connector-python is free software: you can redistribute it and/or modify
-# it under the terms of the Apache License, Version 2.0 (the "License").
-#
-# visier-connector-python is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# Apache License, Version 2.0 for more details.
-#
-# You should have received a copy of the Apache License, Version 2.0
-# along with visier-connector-python. If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+# Copyright 2023 Visier Solutions Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 Basic Authentication class for Visier Connector
